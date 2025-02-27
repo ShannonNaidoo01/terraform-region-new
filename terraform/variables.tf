8b3ed5eed86600e07909724c2c45e6c5696ce012
Shannon Naidoo <shannon.naidoo+GGL@gamesglobal.com>
2025-02-27 14:53:26 +0200

    new code change

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "subscription_id" {
+  description = "The ID of the subscription"
+  type        = string
+}
+
+variable "subscription_name" {
+  description = "The name of the subscription"
+  type        = string
+}
