b17602caebb3dcf6f1638b05412e89b2afbdd976
Shannon Naidoo <166831027+ShannonNaidoo01@users.noreply.github.com>
2025-02-27 17:10:10 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "policy-terraformstate-ports-001"
+    resource_group_name  = "policy-terraformstate-ports-002"
     storage_account_name = "policyterraports002"
     container_name       = "terraformstate"
     key                  = "terraform.region.tfstate"
